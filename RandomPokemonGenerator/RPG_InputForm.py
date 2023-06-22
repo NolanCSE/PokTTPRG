9698e67cb7bac35bbbdc775f5a61b9d12bcb054f
NolanCSE <nolanalbanese@gmail.com>
2023-06-21 23:26:28 -0400

    added some tkinter stuff

--- RandomPokemonGenerator/RPG_InputForm.py
+++ RandomPokemonGenerator/RPG_InputForm.py
@@ -3,51 +3,51 @@ from tkinter import ttk
 import time
 import yaml
 
-class InputForm():
-    __pokedex = []
-    __environments = ["ARCTIC", "BEACH", "CAVE", "DESERT", "FOREST", "FRESHWATER", "GRASSLANDS", "MARSH", "MOUNTAIN", "OCEAN", "RAINFOREST", "TAIGA", "TUNDRA", "URBAN"]
-    __levelBoundary = [0, 100]
-    __evolRange = [1, 3]
-    __types = ["NORMAL", "FIRE", "WATER", "ELECTRIC", "GRASS", "ICE", "FIGHTING", "POISON", "GROUND", "FLYING", "PSYCHIC", "BUG", "ROCK", "GHOST", "DARK", "STEEL", "FAIRY", "DRAGON"]
-    __quantity = 1
+#Config
+ENVIRONMENTS = ["ARCTIC", "BEACH", "CAVE", "DESERT", "FOREST", "FRESHWATER", "GRASSLANDS", "MARSH", "MOUNTAIN", "OCEAN", "RAINFOREST", "TAIGA", "TUNDRA", "URBAN"]
+LEVEL_BOUNDARY = [0, 100]
+EVOLUTION_RANGE = [1, 3]
+TYPES = ["NORMAL", "FIRE", "WATER", "ELECTRIC", "GRASS", "ICE", "FIGHTING", "POISON", "GROUND", "FLYING", "PSYCHIC", "BUG", "ROCK", "GHOST", "DARK", "STEEL", "FAIRY", "DRAGON"]
 
-    def __init__(self):
-        self.open()
+#Data
+pokedex = []
 
-    def open(self):
-        #window = Tk()
-        self.openLoader()
+#instance vars subject to change
+quantity = 1
+environments_to_generate = []
+acceptable_levels = []
+acceptable_evol_stages = []
+acceptable_types = []
 
-        #Build form        
-        self.buildBasicSection()
-        #window.mainloop()
+def create_basic_section(frame : ttk.Frame):
+    quantity_label = ttk.Label(frame, text="Quantity")
+    quantity_label.pack(padx=5, pady=5)
 
-    def buildBasicSection(self):
-        pass
+    quantity_box = ttk.Spinbox(frame, increment=1, to=20)
+    quantity_box.pack(padx=5, pady=5)
+    pass
 
-    def helper(self, window : Tk):
-        print("before")
-        window.after(5000, self.closeWindow(window))
-        print("here")
+def create_form():
+    global LEVEL_BOUNDARY, ENVIRONMENTS, EVOLUTION_RANGE, TYPES, pokedex, quantity
+    window = Tk(className=" Random Pokemon Generator", height=500, width=500)
+    frame = ttk.Frame(window)
+    frame.grid(row=0, column=0)
+    create_basic_section(frame=frame)
 
+    window.mainloop()
 
-    def openLoader(self):
-        loader = Tk()
-        progress = ttk.Progressbar(loader, orient=HORIZONTAL, length=300, mode='indeterminate')
-        progress.pack(pady=20)
-        progress.start(10)
-
-        self.helper(progress)
-        loader.mainloop()
-
-    def closeWindow(self, window : Tk):
-        window.destroy()
-        window.quit()
-
+def load_pokedex():
+    global pokedex
+    with open("pokedex_rpg.yml", "r") as poke_file:
+        pokedex = yaml.safe_load(poke_file)
 
 
 def main():
-    form = InputForm()
-    pass
+    global pokedex
+    load_pokedex()
+    print(pokedex)
+    create_form()
+
 if __name__ == "__main__":
     main()
+
