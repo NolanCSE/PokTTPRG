2af3d6cfcc034eda027d7a944112d11f9e391a9d
NolanCSE <nolanalbanese@gmail.com>
2023-06-22 16:52:45 -0400

    added more GUI stuff

--- RandomPokemonGenerator/RPG_Controller.py
+++ RandomPokemonGenerator/RPG_Controller.py
@@ -1,2 +1,13 @@
 from RPG_InputForm import *
 
+#instance vars subject to change
+quantity = 1
+environments_to_generate = []
+acceptable_levels = []
+acceptable_evol_stages = []
+
+def updateQuantity(q : int):
+    global quantity
+    print("Updating Quantity: {}".format(q))
+    quantity = q
+    pass
